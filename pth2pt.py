4fa634a33a2b9b184bba133d085c2b8e2621d497
okabe <923048992@qq.com>
2021-09-27 11:13:23 +0800

    fix pth2pt

--- pth2pt.py
+++ pth2pt.py
@@ -3,13 +3,7 @@ import torchvision
 from Models import U_Net
 
 pthUrl = r'D:/Unet-Segmentation-Pytorch-Nest-of-Unets/model/Unet_D_15_4/Unet_epoch_15_batchsize_4.pth'
-
-<<<<<<< HEAD
 ptUrl = r'D:/Unet-Segmentation-Pytorch-Nest-of-Unets/model/Unet_D_15_4/model.ptl'
-
-import torch
-import torchvision
-from torch.utils.mobile_optimizer import optimize_for_mobile
 
 model = U_Net(3, 1)
 model.load_state_dict(torch.load(pthUrl))
@@ -18,14 +12,3 @@ example = torch.rand(1, 3, 608, 608)
 traced_script_module = torch.jit.trace(model, example)
 traced_script_module_optimized = optimize_for_mobile(traced_script_module)
 traced_script_module_optimized._save_for_lite_interpreter(ptUrl)
-=======
-ptUrl = r'D:/Unet-Segmentation-Pytorch-Nest-of-Unets/model/Unet_D_15_4/Unet_epoch_15_batchsize_4.pt'
-
-model = U_Net(3, 1)
-print(pthUrl)
-test1 = model.load_state_dict(torch.load(pthUrl))
-model.eval()
-example = torch.rand(4, 1, 608, 608)
-traced_module = torch.jit.trace(model, example)
-traced_module.save(ptUrl)
->>>>>>> 44dda492b4b9f11423cf8278f51c673acf8b219e
